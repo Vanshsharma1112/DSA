6efb089c2735b6a7a05a475b265cb4ed0db0809f
Vanshsharma1112 <107877142+Vanshsharma1112@users.noreply.github.com>
2022-10-09 16:07:28 +0530

    Add files via upload

---
+++ DSA Assignment 4/Apple and Oranges.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int INF = 0x3f3f3f3f;
+const int mod = 1e9+7;
+const double eps = 1e-8;
+const int maxn = 112345;
+
+
+int main()
+{
+   
+
+    int s,t,a,b,m,n,x;
+    int aa,bb;
+    aa = bb = 0;
+
+    scanf("%d%d%d%d%d%d",&s,&t,&a,&b,&m,&n);
+    while(m--)
+    {
+        scanf("%d",&x);
+        if(s <= a+x && a+x <= t) aa++;
+    }
+
+    while(n--)
+    {
+        scanf("%d",&x);
+        if(s <= b+x && b+x <= t) bb++;
+    }
+
+    printf("%d\n%d\n",aa,bb);
+
+    return 0;
+}
